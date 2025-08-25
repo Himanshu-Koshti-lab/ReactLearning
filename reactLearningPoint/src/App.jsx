a140029de4db04733dcecf074df1cb978a729a66
Himanshu Koshti <Himfeedback74@gmail.com>
2025-08-25 20:35:45 +0530

    Add buttons

--- reactLearningPoint/src/App.jsx
+++ reactLearningPoint/src/App.jsx
@@ -1,18 +1,19 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import MyButton from './MyButton'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
     <div className='card'>
       <p>Hello React {count}</p>
       <button onClick={() => setCount((count) => count + 1)}>Click Here</button>
+      <br /><br />
+      <MyButton />
+      <br /><br />
+      <button>Hello</button>
     </div>
-    </>
   )
 }
 
---
+++ reactLearningPoint/src/MyButton.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './App.css'
+
+export default function MyButton() {
+  return (
+    <button>My Button</button>
+  )
+}
