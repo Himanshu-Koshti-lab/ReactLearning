3605e3519303676c25f66d339acf0cfa625aab6a
Himanshu Koshti <Himfeedback74@gmail.com>
2025-08-25 20:48:35 +0530

    object in react

--- reactLearningPoint/src/MyButton.jsx
+++ reactLearningPoint/src/MyButton.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { use } from 'react'
 import './App.css'
 
 export default function MyButton() {
+
+    let name = "Himanshu Koshti";
+
+    const user = {
+        "name" : "Himanshu Koshti",
+        "exp" : "5+",
+        "age" : 27,
+    };
+
   return (
-    <button className='avatarButton'> My Button</button>
+    <>
+    <button className='avatarButton'> My name is {user.name} </button>
+    <p>Experience is  {user.exp} and age is {user.age}</p>
+    </>
+
   )
 }
