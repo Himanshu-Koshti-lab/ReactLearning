2298933dad7732651b9dc4cd82886a64893aead1
Himanshu Koshti <Himfeedback74@gmail.com>
2025-08-26 20:40:00 +0530

    tictactoe game

--- reactLearningPoint/src/App.jsx
+++ reactLearningPoint/src/App.jsx
@@ -3,9 +3,18 @@ import { useState } from "react";
 import MyButton from "./MyButton";
 import AdminCard from "./AdminCard";
 import UserCard from "./UserCard";
+import Game from "./tictactoe";
+import Square from "./tictactoe";
+import Board from "./tictactoe";
 
 function App() {
   const [count, setCount] = useState(0);
+
+    const user = {
+    name: "Himanshu Koshti",
+    exp: "5+",
+    age: 27,
+  };
 
   let isAdmin = false;
 
@@ -13,14 +22,27 @@ function App() {
 
   if (isAdmin) showCard = <AdminCard />;
 
+  const [clickCount, setClickCount] = useState(0);
+
+  function showAlert() {
+    setClickCount(clickCount + 1);
+  }
+
   return (
     <div className="card">
-      <p>Hello React {count}</p>
+      {/* <button className="avatarButton"> My name is {user.name} </button>
+      <p>
+        Experience is {user.exp} and age is {user.age}
+      </p>
+      <p>Hello React <br /> App level counter {count} <br /> Parent + ChildLevel Counter {clickCount} </p>
       <button onClick={() => setCount((count) => count + 1)}>Click Here</button>
       <br />
-      <MyButton />
+      <MyButton count = {clickCount} functionPass={showAlert}/>
+      <MyButton count = {clickCount} functionPass={showAlert}/>
       <br />
-      {showCard}
+      {showCard} */}
+
+      <Board />
     </div>
   );
 }
--- reactLearningPoint/src/MyButton.jsx
+++ reactLearningPoint/src/MyButton.jsx
@@ -1,30 +1,9 @@
-import React, { use } from "react";
 import "./App.css";
-import { useState } from "react";
 
-export default function MyButton() {
-  let name = "Himanshu Koshti";
-
-  const [clickCount, setClickCount] = useState(0);
-
-  const user = {
-    name: "Himanshu Koshti",
-    exp: "5+",
-    age: 27,
-  };
-
-  function showAlert() {
-    setClickCount(clickCount + 1);
-    alert("You clicked me..." + clickCount);
-  }
-
+export default function MyButton({count, functionPass}) {
   return (
     <>
-      <button className="avatarButton"> My name is {user.name} </button>
-      <p>
-        Experience is {user.exp} and age is {user.age}
-      </p>
-      <button onClick={showAlert}>Dare Click me... {clickCount}</button>
+      <button onClick={functionPass}>Dare Click me... {count}</button>
     </>
   );
 }
