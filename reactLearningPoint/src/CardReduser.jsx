a064a8f6c044894dcd42a93f4df59daeef8eddb1
Himanshu Koshti <Himfeedback74@gmail.com>
2025-08-27 13:01:36 +0530

    total operation count by useEffect

--- reactLearningPoint/src/CardReduser.jsx
+++ reactLearningPoint/src/CardReduser.jsx
@@ -1,4 +1,4 @@
-import React, { useReducer } from "react";
+import React, { useEffect, useReducer, useRef } from "react";
 
 const ACTION = {
   INCREASE: "add",
@@ -6,33 +6,46 @@ const ACTION = {
 };
 
 export default function CardReduser() {
-  const [state, doOperation] = useReducer(operation, {
+
+
+  const [stateMeansCollOfObj, joMethodCallKrneUseHo] = useReducer(joMethodHai, {
+//    Initial Values 
     count: 0,
     operationPerform: 0,
   });
 
+  const count = useRef(0)
+
+  useEffect(() => {
+    count.current = count.current + 1;
+  });
+
+  
+  const increment  = () => joMethodCallKrneUseHo({ type: ACTION.INCREASE })
+  const decrement  = () => joMethodCallKrneUseHo({ type: ACTION.DECREASE})
+
   return (
     <div>
-      <h2>Count: {state.count}</h2>
-      <h2>Total Operations : {state.operationPerform}</h2>
-      <button onClick={() => doOperation({ type: ACTION.INCREASE })}>+</button>
-      <button onClick={() => doOperation({ type: ACTION.DECREASE })}>-</button>
-      <button onClick={() => doOperation({ type: "reset" })}>Reset</button>
+      <h2>Count: {stateMeansCollOfObj.count}</h2>
+      <h2>Total Operations : {count.current}</h2>
+      <button onClick={increment}>+</button>
+      <button onClick={decrement}>-</button>
+      <button onClick={() => joMethodCallKrneUseHo({ type: "reset" })}>Reset</button>
     </div>
   );
 }
 
-function operation(state, action) {
-  switch (action.type) {
+function joMethodHai(joCurrentStateHObjectKi, krnaKyaH) {
+  switch (krnaKyaH.type) {
     case ACTION.INCREASE:
       return {
-        count: state.count + 1,
-        operationPerform: state.operationPerform + 1,
+        count: joCurrentStateHObjectKi.count + 1,
+        operationPerform: joCurrentStateHObjectKi.operationPerform + 1,
       };
     case ACTION.DECREASE:
       return {
-        count: state.count - 1,
-        operationPerform: state.operationPerform + 1,
+        count: joCurrentStateHObjectKi.count - 1,
+        operationPerform: joCurrentStateHObjectKi.operationPerform + 1,
       };
     case "reset":
       return { count: 0, operationPerform: 0 };
