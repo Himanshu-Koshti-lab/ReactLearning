3a6e81939e9c96406f15baa6706a9f98336563fd
Himanshu Koshti <Himfeedback74@gmail.com>
2025-08-27 20:51:33 +0530

    Define Card

---
+++ carder/src/Card.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import "./Card.css"
+
+const product = {
+    "id": 1,
+    "title": "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+    "price": 109.95,
+    "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+    "category": "men's clothing",
+    "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png",
+    "rating": {
+      "rate": 3.9,
+      "count": 120
+    }
+};
+
+export default function Card() {
+  return (
+    <div className="card">
+       <img src={product.image} />
+       <h3 className="title">{product.title}</h3>
+       <h4 className="price">{product.price}</h4>
+       <button>More Info</button> 
+    </div>
+  )
+}
