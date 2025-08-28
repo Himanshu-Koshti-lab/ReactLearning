3eed19ea4723d54e1dd8d94b3a71cdb521850384
Himanshu Koshti <Himfeedback74@gmail.com>
2025-08-28 23:49:47 +0530

    H1 tag blink and typing effect

--- carder/src/App.jsx
+++ carder/src/App.jsx
@@ -1,14 +1,13 @@
-import './App.css'
-import Card from './Card'
+import "./App.css";
+import Card from "./Card";
 
 function App() {
-
   return (
     <>
       <h1> Product Card</h1>
       <Card />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
