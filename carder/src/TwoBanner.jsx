3acb243da3141984a422b9c385540ec53bc66e53
Himanshu Koshti <Himfeedback74@gmail.com>
2025-09-01 20:44:54 +0530

    using SetInterval to  sutomatic move to next slide

--- carder/src/TwoBanner.jsx
+++ carder/src/TwoBanner.jsx
@@ -1,18 +1,24 @@
-import React, { useState } from "react";
-import "./TwoBanner.css"
+import React, { useEffect, useState } from "react";
+import "./TwoBanner.css";
 
 const slides = [
   {
     id: 1,
     img: "https://picsum.photos/1200/400?random=10",
-    title: "First Banner",
+    title: "Slide 1",
     desc: "This is the first banner",
   },
   {
     id: 2,
     img: "https://picsum.photos/1080/720?grayscale",
-    title: "Second Banner",
+    title: "Slide 2",
     desc: "This is the second banner",
+  },
+  {
+    id: 3,
+    img: "https://picsum.photos/1200/400?random=103",
+    title: "Slide 3",
+    desc: "Here’s the third slide 🎉",
   },
 ];
 
@@ -27,15 +33,30 @@ export default function TwoBanner() {
     setCurrent((prev) => (prev - 1 + slides.length) % slides.length);
   };
 
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCurrent((prev) => (prev + 1) % slides.length);
+    }, 2000);
+    return () => clearInterval(timer);
+  }, []);
+
   return (
-    <div className="banner">
-      <img src={slides[current].img} alt={slides[current].title} className="banner-image2"/>
+    <div className="banner" key={slides[current].id}>
+      <img
+        src={slides[current].img}
+        alt={slides[current].title}
+        className="banner-image2"
+      />
       <div className="banner-text">
         <h2>{slides[current].title}</h2>
         <p>{slides[current].desc}</p>
       </div>
-      <button className="prev" onClick={prevSlide}>prev</button>
-      <button className="next" onClick={nextSlide}>Next</button>
+      <button className="prev" onClick={prevSlide}>
+        prev
+      </button>
+      <button className="next" onClick={nextSlide}>
+        Next
+      </button>
     </div>
   );
 }
