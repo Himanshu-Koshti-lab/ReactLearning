193583ca4cd9af3329392bb182f67f47e7dbc694
Himanshu Koshti <Himfeedback74@gmail.com>
2025-09-01 19:18:52 +0530

    First Banner

--- carder/src/Banner.jsx
+++ carder/src/Banner.jsx
@@ -3,6 +3,11 @@ import "./Banner.css"
 
 export default function Banner() {
   return (
-    <div className='Banner'> Banner</div>
+    <div className='Banner'>
+      <img src="https://picsum.photos/1200/400" alt="Image"  className='Banner-Image'/>
+      <div className="banner-text">
+        <h2>My First Banner</h2>
+        <p>How it is ?</p></div> 
+    </div>
   )
 }
